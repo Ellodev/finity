8aec6d2b180b565c6dc7af139a0a8277853b2f5e
Ellodev <ello.developers@gmail.com>
2023-11-02 11:44:47 +0100

    fix: win defintion feat: explanation feat: timer

--- memory-matching-game/script.js
+++ memory-matching-game/script.js
@@ -21,6 +21,19 @@ const selectedItems = [];
 let openCards = [];
 let completedSets = 0;
 let tries = 0;
+let clickable = true;
+let seconds = 0;
+
+document
+  .getElementById("explanation-button")
+  .addEventListener("click", startGame);
+
+function startGame() {
+  document.getElementById("explanation").setAttribute("hidden", "true");
+  document.getElementById("timer").removeAttribute("hidden");
+  document.getElementById("game-div").removeAttribute("hidden");
+  startTimer();
+}
 
 // Loop until the original array is empty
 while (emojis.length > 0) {
@@ -41,14 +54,24 @@ while (emojis.length > 0) {
 // check if 2 ites are open
 // check if they are the same => mark as comeplete
 
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 const betterListener = (event) => {
   let target = event.target;
+
+  if (target === "") return;
+
+  if (clickable === false) return;
+  console.log(clickable);
 
   if (target.hasAttribute("completed")) return;
 
   if (openCards.find((openCardId) => openCardId === target.id)) return;
 
   target.textContent = selectedItems[target.id];
+  openCards.push(target.id);
 
   if (openCards.length > 1) {
     let openCardA = document.getElementById(openCards[0]);
@@ -61,65 +84,44 @@ const betterListener = (event) => {
       openCardB.setAttribute("completed", "");
       completedSets++;
     } else {
-      openCardA.textContent = "";
-      openCardB.textContent = "";
+      clickable = false;
+      console.log(clickable);
+      sleep(1000).then(() => {
+        openCardA.textContent = "";
+        openCardB.textContent = "";
+        clickable = true;
+        console.log(clickable);
+      });
     }
 
     openCards = [];
   }
 
-  openCards.push(target.id);
-
   console.log({ completedSets, tries });
 
   if (completedSets > 7) {
-    alert(`You completed the game in ${tries} and in `);
-    // TODO add timer
+    alert(`You completed the game in ${tries} tries and in ${seconds} seconds`);
     // TODO add reset function
   }
 };
 
 document.getElementById("game-table").addEventListener("click", betterListener);
 
-// original code:
+// Get the current time in milliseconds when the page is loaded
+function startTimer() {
+  let startTime = new Date().getTime();
+  // Pass the startTime variable to the setInterval function
+  setInterval(showSeconds, 1000, startTime);
+}
 
-// document
-//   .getElementById("game-table")
-//   .addEventListener("click", function (event) {
-//     let target = event.target; // Get the element that was clicked
-
-//     // Check if the clicked element is a table cell (td)
-//     if (
-//       target.tagName === "TD" &&
-//       !target.hasAttribute("completed") &&
-//       openCards[0] !== target.id &&
-//       openCards[1] !== target.id
-//     ) {
-//       //Make the textContent of the clicked Item the emoji from selectedItems array
-//       if (openCards.length > 1) {
-//         // two cards are open, we compare
-
-//         let openCardA = document.getElementById(openCards[0]);
-//         let openCardB = document.getElementById(openCards[1]);
-
-//         if (openCardA.textContent === openCardB.textContent) {
-//           openCardA.setAttribute("completed", "");
-//           openCardB.setAttribute("completed", "");
-
-//           openCards.length = 0;
-//           completedSets = completedSets + 1;
-//         } else {
-//           openCardA.textContent = "";
-//           openCardB.textContent = "";
-//           openCards.length = 0;
-//         }
-//       }
-
-//       openCards.push(target.id);
-//       target.textContent = selectedItems[target.id];
-
-//       if (completedSets > 6) {
-//         alert("You won!");
-//       }
-//     }
-//   });
+// Define a function that updates the display every second
+function showSeconds(startTime) {
+  // Get the current time in milliseconds
+  let currentTime = new Date().getTime();
+  // Calculate the difference in milliseconds using the startTime argument
+  let diff = currentTime - startTime;
+  // Convert the difference to seconds
+  seconds = Math.floor(diff / 1000);
+  // Display the seconds in an element with id "timer"
+  document.getElementById("timer").innerHTML = seconds + " seconds";
+}
