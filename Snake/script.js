96ef9e3989e145ef7d6c1fb1f3681b2f809244f4
Tan <j.tan@edu.schule-root.ch>
2023-11-09 11:35:34 +0100

    Snake
    Snake game finished, without restart button

--- Snake/script.js
+++ Snake/script.js
@@ -1,7 +1,6 @@
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
-
 class SnakePart{
     constructor(x, y){
         this.x = x;
@@ -12,10 +11,10 @@ class SnakePart{
 let speed = 7;
 
 let tileCount = 16;
-let tileSize = canvas.width / tileCount -2;
+let tileSize = canvas.width / tileCount;
 
-let headX = 8;
-let headY = 8;
+let headX = 10;
+let headY = 10;
 const snakeParts = [];
 let tailLength = 2;
 
@@ -25,15 +24,77 @@ let appleY = 5;
 let xVelocity = 0;
 let yVelocity = 0;
 
+let score = 0;
+
 //game loop
 function drawGame(){
-    clearScreen();
+
     changeSnakePosition();
+    let result = isGameOver();
+    if(result){
+        return;
+    }
+
+    clearScreen();
 
     checkAppleCollision();
     drawApple();
     drawSnake();
+
+    drawScore();
+
     setTimeout(drawGame, 1000/ speed);
+}
+
+function isGameOver(){
+    let gameOver = false;
+
+    if(yVelocity === 0 && xVelocity === 0){
+        return false;
+    }    
+
+    //walls
+    if(headX < 0 ){
+        gameOver = true;
+    }
+    if(headX >= 20){
+        gameOver = true;
+    }
+    else if(headY < 0){
+        gameOver = true;
+    }
+    else if(headY === 20){
+        gameOver = true;
+    }
+
+    for(let i =0; i < snakeParts.length; i++){
+        let part = snakeParts[i];
+        if(part.x === headX && part.y === headY){
+            gameOver = true;
+            break;
+        }
+    }
+
+    if (gameOver) {
+        ctx.fillStyle = "white";
+        ctx.font = "40px Verdana";
+
+        var gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
+        gradient.addColorStop("0", "white");
+        gradient.addColorStop("0.5", "blue");
+        gradient.addColorStop("1.0", "white");
+        // Fill with gradient
+        ctx.fillStyle = gradient;
+
+        ctx.fillText("Game Over!", canvas.width / 9, canvas.height / 2);
+    }
+    return gameOver;
+}
+
+function drawScore(){
+    ctx.fillstyle = 'white';
+    ctx.font = "10px Verdana";
+    ctx.fillText("Score:" + score, canvas.width-50, 10);
 }
 
 function clearScreen(){
@@ -50,7 +111,7 @@ function drawSnake(){
     }
 
     snakeParts.push(new SnakePart(headX, headY));
-    if(snakeParts.length > tailLength){
+    while(snakeParts.length > tailLength){
         snakeParts.shift();
     }
 
@@ -75,48 +136,41 @@ function checkAppleCollision(){
         appleX = Math.floor(Math.random() * tileCount);
         appleY = Math.floor(Math.random() * tileCount);
         tailLength++;
+        score++;
     }
 }
 
 document.body.addEventListener("keydown", keyDown);
 
-function keyDown(event){
-    //up
-    if(event.keyCode === 38){
-        if(yVelocity === 1){
-            return;
-        } 
-        yVelocity = -1;
-        xVelocity = 0;
-    }
-
-    //down
-    if(event.keyCode === 40){
-        if(yVelocity === -1){
-            return;
-        }
-        yVelocity = 1;
-        xVelocity = 0;
-    }
-
-    //left
-    if(event.keyCode === 37){
-        if(xVelocity === 1){
-            return;
-        }
-        yVelocity = 0;
-        xVelocity = -1;
-    }
-
-    //right
-    if(event.keyCode === 39){
-        if(xVelocity === -1){
-            return;
-        }
-        yVelocity = 0;
-        xVelocity = 1;
+function keyDown(event) {
+    switch (event.keyCode) {
+        case 38: // Up
+            if (yVelocity !== 1) {
+                yVelocity = -1;
+                xVelocity = 0;
+            }
+            break;
+        case 40: // Down
+            if (yVelocity !== -1) {
+                yVelocity = 1;
+                xVelocity = 0;
+            }
+            break;
+        case 37: // Left
+            if (xVelocity !== 1) {
+                yVelocity = 0;
+                xVelocity = -1;
+            }
+            break;
+        case 39: // Right
+            if (xVelocity !== -1) {
+                yVelocity = 0;
+                xVelocity = 1;
+            }
+            break;
     }
 }
+
 
 
 drawGame();
