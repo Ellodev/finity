91822da24a20a26e4bf0f4c883ec36cfeb5727db
Ellodev <ello.developers@gmail.com>
2023-11-16 11:42:52 +0100

    started working on jump and run

---
+++ Jump-n-run/script.js
@@ -0,0 +1,9 @@
+// Code mostly/partially from https://www.youtube.com/watch?v=bG2BmmYr9NQ
+
+let character = document.getElementById("character");
+let block = document.getElementById("block");
+
+function jump(){
+    character.classList.add("animate");
+    console.log("you have reached function jump")
+}
